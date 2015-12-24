e264dc3d7d8d9909a414979a041c2710a9243711
Borja Gomez Gimeno <borja@altealabs.local>
2015-12-24 14:56:57 +0100

    subo proyecto azureTableStorage

---
+++ AzureTableStorage/AzureTableStorage/Folder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTableStorage
+{
+    public class Folder
+    {
+        public List<Folder> SubFolders { get; set; }
+    }
+
+    public class FileMetadata
+    {
+        public string FileName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string BlobReferenceKey { get; set; }
+    }
+}
